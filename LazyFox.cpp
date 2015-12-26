4ad729f763682c539063d46cbe7f8b714be39421
Diana Chang <diana.chang.twn@gmail.com>
2015-12-26 18:25:04 -0500

    interesting DP solution

--- LazyFox.cpp
+++ LazyFox.cpp
@@ -1,55 +1,55 @@
 //Leo's code
 //nice DP solution
-#include <iostream>
-#include <algorithm>
-
-int candy[2002][2];
-int dist[2002][2000];
-int dp[2002][5000];
-
+#include<bits/stdc++.h>
 using namespace std;
 
+#define mp make_pair
+#define pb push_back
+#define PII pair<int,int>
+#define PIPII pair<int,pair<int,int>>
+#define MAXN 2010
+
+PII points[MAXN];
+vector<PIPII> dp;
+int dist[MAXN],candy[MAXN],pcandy[MAXN],N,d,a,b;
+
+int calc(int i,int j){
+    int distx,disty;
+    distx = points[i].first-points[j].first;
+    disty = points[i].second-points[j].second;
+    return distx*distx + disty*disty;
+}
 int main()
 {
 	int N;
 	cin >> N;
-	for (int i = 0; i != N; ++i)
-		cin >> candy[i][0] >> candy[i][1];
-	for (int i = 0; i != N; ++i)
-	{
-		for (int j = 0; j != N; ++j)
-			dist[i][j] = (candy[i][0] - candy[j][0])*(candy[i][0] - candy[j][0]) + (candy[i][1] - candy[j][1])*(candy[i][1] - candy[j][1]);
-	}
-	for (int i = 0; i != N; ++i)
-		dp[i][1] = (candy[i][0] * candy[i][0]) + (candy[i][1] * candy[i][1]);
+	points[0].first=0;
+    points[0].second=0;
+	for (int i = 1; i <= N; ++i)
+		cin >> points[i].first>>points[i].second;
 
-	int count = 1, tmx = 1, cont = 1;
-	while (cont)
-	{
-		cont = 0;
-		for (int i = 0; i != N; ++i)
-		{
-			for (int j = 0; j != N; ++j)
-			{
-				if (dp[i][count] > dist[i][j] && dist[i][j] != 0)
-				{
-					dp[j][count + 1] = max(dp[j][count + 1], dist[i][j]);
-					tmx = max(tmx, count + 1);
-					cont = 1;
-				}
-			}
-		}
-		++count;
+	for (int i=0;i<=N;i++){
+        for (int j=i+1;j<=N;j++){
+            dp.pb(mp(calc(i,j),mp(i,j)));
+        }
 	}
+	sort(dp.begin(),dp.end());
 
-	for (int i = 0; i != N; ++i)
-	{
-		for (int j = 0; j != N; ++j)
-		{
-			cout << dist[i][j] << " ";
-		}
-		cout << endl;
+	for (int i=0;i<dp.size();i++){
+        d = dp[i].first; a = dp[i].second.first; b = dp[i].second.second;
+        if (d!=dist[a]){
+            pcandy[a]=candy[a];
+            dist[a]=d;
+        }
+        if (d!=dist[b]){
+            pcandy[b]=candy[b];
+            dist[b]=d;
+        }
+        candy[a] = max(candy[a],pcandy[b]+1);
+        if (a!=0){
+            candy[b] = max(candy[b],pcandy[a]+1);
+        }
 	}
-	cout << tmx;
+	cout<<candy[0]<<endl;
 	return 0;
 }
