720d1707c7b5804525bfe1680c71eae64255f6ef
Diana Chang <diana.chang.twn@gmail.com>
2016-02-14 20:45:03 -0500

    clever use of sets, added problem description

--- ccc15s3.cpp
+++ ccc15s3.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 {
     cin.sync_with_stdio(0); cin.tie(0);
-    int G, P; cin >> G >> P;
+    int G, P; cin >> G >> P; //number of gates, number of planes to park
     set<int> gates;
     for (int i=1; i<=G; ++i) gates.insert(i); //insert the spots in the gates set
     int ans = 0;
     while (ans < P){
         int p;
-        cin >> p;
+        cin >> p; //plane number to be parked
         //gates.upper_bound(p) returns the least value in gates that is greater than p
         //if that upper_bound(p) is where the set begins
         //there are no spots that can be parked (no spots less than or equal to p)
