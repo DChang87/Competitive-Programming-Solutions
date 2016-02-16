a771261a5e87b198ae81bbe0daff27f2369ef0b5
Diana Chang <diana.chang.twn@gmail.com>
2016-02-16 13:50:14 -0500

    bfs, hashing, added problem description

--- ccc12s4.cpp
+++ ccc12s4.cpp
@@ -1,17 +1,27 @@
 //BFS, hashing
+/*
+When she is bored, Jo Coder likes to play the following game with coins on a table. She takes a set of distinct coins and 
+lines them up in a row. For example, let us say that she has a penny (P, worth $0.01), a nickel (N, worth $0.05), and a dime 
+(D, worth $0.10). She lines them up in an arbitrary order (for example, D N P), and then moves them around with the goal of 
+placing them in strictly increasing order by value, that is P N D (i.e., $0.01, $0.05, $0.10). She has particular rules that she follows:
+The initial coin line-up defines all positions where coins can be placed. That is, no additional positions can be added later, 
+and even if one of the positions does not have a coin on it at some point, the position still exists.
+The game consists of a sequence of moves and in each move Jo moves a coin from one position to an adjacent position.
+The coins can be stacked, and in a move Jo always takes the top coin from one stack and moves it to the top of another stack.
+In a stack of coins, Jo never places a higher-value coin on top of a lower-value coin.
+For simplicity, let the coins have consecutive integer values (e.g., denote the penny as 1, nickel as 2, and dime as 3). 
+May be impossible
+*/
 #include<bits/stdc++.h>
 using namespace std;
 set<int> visited;
 int N, n, targetCode,hashCode,ans;
-
-
 int pow(int base, int exp)
 {
 	int x = 1;
 	for (int i = 0; i < exp; i++) x *=base;
 	return x;
 }
-
 int bfs(int hashCode)
 {
     queue<pair<int,int>> q; //pair<hashCode,numMoves>
@@ -54,7 +64,6 @@ int bfs(int hashCode)
     }
     return -1;
 }
-
 int main()
 {
     cin.sync_with_stdio(0);cin.tie(0);
@@ -71,12 +80,8 @@ int main()
             targetCode += i*pow(8,i);
         }
         ans = bfs(hashCode);
-        if (ans>=0){
-            cout<<ans<<endl;
-        }
-        else{
-            cout<<"IMPOSSIBLE"<<endl;
-        }
+        if (ans>=0) cout<<ans<<endl;
+        else cout<<"IMPOSSIBLE"<<endl;
         cin>>N;
     }
     return 0;
