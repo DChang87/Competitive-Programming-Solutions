26fd1e23ff46b981d39e57646da7ad5514c1de72
Diana Chang <diana.chang.twn@gmail.com>
2016-02-16 14:08:51 -0500

    geo, sweepline, added problem description

--- ccc09s5.cpp
+++ ccc09s5.cpp
@@ -1,14 +1,18 @@
 /*
-Bob is sitting at home with his computer. He would like to experience more social interaction, so he is planning a trip to a coffee shop with his computer.
-
-Bob has lots of data about wireless networks and coffee shops in the city. In Bob's city, there is one coffee shop at every intersection of streets. Specifically, Bob happens to live in a city with M streets (1 ≤ M ≤ 30,000) that run east and west, and N streets (1 ≤ N ≤ 1,000) that run north and south. As an added benefit, the distance between consecutive parallel streets is 1 metre (it is a very compact city).
-
-It also turns out that inside K (1 ≤ K ≤ 1,000) of the coffee shops, there is a wireless network station. Each wireless network station will have a particular bitrate B (1 ≤ B ≤ 1,000) and can reach R metres (1 ≤ R ≤ 30,000) from the coffee shop. In other words, a wireless network station from one coffee shop forms a circle with radius R centered at that particular coffee shop. Moreover, if someone is at distance R, the wireless network would be available, but if someone is at a distance larger than R, they cannot access that wireless point.
-
-You can assume that each coffee shop has at most one wireless network stationed in it, but that multiple wireless networks may be available while sitting in that one coffee shop, due to the proximity of other wireless network stations.
-
+Bob is sitting at home with his computer. He would like to experience more social interaction, 
+so he is planning a trip to a coffee shop with his computer.
+Bob has lots of data about wireless networks and coffee shops in the city. In Bob's city, there is one coffee shop 
+at every intersection of streets. Specifically, Bob happens to live in a city with M streets (1 ≤ M ≤ 30,000) that 
+run east and west, and N streets (1 ≤ N ≤ 1,000) that run north and south. As an added benefit, the distance between 
+consecutive parallel streets is 1 metre (it is a very compact city).
+It also turns out that inside K (1 ≤ K ≤ 1,000) of the coffee shops, there is a wireless network station. Each wireless 
+network station will have a particular bitrate B (1 ≤ B ≤ 1,000) and can reach R metres (1 ≤ R ≤ 30,000) from the coffee 
+shop. In other words, a wireless network station from one coffee shop forms a circle with radius R centered at that 
+particular coffee shop. Moreover, if someone is at distance R, the wireless network would be available, but if someone 
+is at a distance larger than R, they cannot access that wireless point.
+You can assume that each coffee shop has at most one wireless network stationed in it, but that multiple wireless networks 
+may be available while sitting in that one coffee shop, due to the proximity of other wireless network stations.
 Bob has a special device in his computer that can use all of the available bitrates of as many wireless networks as he can connect to.
-
 Bob would like to find out the maximum bitrate he can obtain, and how many coffee shops would have that maximum capacity.
 */
 #include<bits/stdc++.h>
