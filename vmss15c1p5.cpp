d15cca7c941a3ff07da575753d9e2559d2d60987
Diana Chang <diana.chang.twn@gmail.com>
2016-02-16 12:18:03 -0500

    3d dp, added problem description

--- vmss15c1p5.cpp
+++ vmss15c1p5.cpp
@@ -1,3 +1,14 @@
+/*
+Frank has made it to a Food Basics! Without causing property damage exceeding $1000! There he meets Jeffrey 
+(since grocery stores don't have roads) and he agrees to help Frank buy some apples. You must also help Frank buy some apples.
+The Food Basics that Frank is visiting has N different types of apples, and he can take as much as he can 
+of each type. However, there are some restrictions: Frank has only R dollars to spend on apples, and his 
+car can only hold S volume, excluding Frank's volume (Frank obviously isn't giving Jeffrey a ride, as cars drive on roads).
+Frank also likes some types of apples more than others, and he assigns a value V to each type of apple. 
+Frank likes apples with larger values.
+Jeffrey and Frank would like to buy apples in such a way so that he can maximise the total sum of all values 
+of apples. Help them calculate it!
+*/
 #include<iostream>
 using namespace std;
 int main()
@@ -20,7 +31,6 @@ int main()
         apples[i][1]=cost;
         apples[i][2] = vol;
     }
-    
     for (int sac=1;sac<=R;sac++){ //cost per sac
         for (int Vol=1;Vol<=S;Vol++){
             for (int app=0;app<N;app++){
@@ -35,7 +45,6 @@ int main()
                 }
             }
         }
-
     }
     cout<<dp[R][S][0]<<endl;
     for (int i=1;i<=N;i++){
