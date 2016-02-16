ecf31213c0f84aba048fdb5d2ca329f956dd2133
Diana Chang <diana.chang.twn@gmail.com>
2016-02-16 14:00:20 -0500

    binary search, moving from position to position with limited fuel, added problem description

--- ccc10s3.cpp
+++ ccc10s3.cpp
@@ -1,14 +1,24 @@
+/*
+There is a very unusual street in your neighbourhood. This street forms a perfect circle, and the circumference of 
+the circle is 1,000,000. There are H (1 ≤ H ≤ 1000) houses on the street. The address of each house is the clockwise 
+arc-length from the northern-most point of the circle. The address of the house at the northern-most point of the circle is 0.
+You also have special firehoses which follow the curve of the street. However, you wish to keep the length of the longest 
+hose you require to a minimum.
+Your task is to place k (1 ≤ k ≤ 1000) fire hydrants on this street so that the maximum length of hose required to 
+connect a house to a fire hydrant is as small as possible.
+Input
+The first line of input will be an integer H, the number of houses. The next H lines each contain one integer, 
+which is the address of that particular house, and each house address is at least 0 and less than 1,000,000. 
+On the H+2nd line is the number k, which is the number of fire hydrants that can be placed around the circle. 
+Note that a fire hydrant can be placed at the same position as a house. You may assume that no two houses are at the same address. 
+*/
 #include<bits/stdc++.h>
 using namespace std;
-int N,K;
-int houses[1010];
-int MAX=1000000;
+int N,K; int houses[1010]; int MAX=1000000;
 int main()
 {
     cin>>N;
-    for (int i=0;i<N;i++){
-        cin>>houses[i];
-    }
+    for (int i=0;i<N;i++) cin>>houses[i];
     cin>>K;
     if (K>=N){
         cout<<0<<endl;
@@ -28,8 +38,7 @@ int main()
         }
         int left=0,right=MAX;
         int hoseLength, hoseCount;
-        //binary search
-        while (right>left+1){
+        while (right>left+1){ //binary search
             hoseCount=1;
             hoseLength = (left+right)/2;
             int hydrantPos = hoseLength;
@@ -39,12 +48,8 @@ int main()
                     hydrantPos = houses[i]+hoseLength;
                 }
             }
-            if (hoseCount<=K){
-                right = hoseLength;
-            }
-            else{
-                left = hoseLength;
-            }
+            if (hoseCount<=K){ right = hoseLength;}
+            else{ left = hoseLength;}
         }
         maxRequired = max(maxRequired,right);
     }
