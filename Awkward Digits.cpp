45cd14a8d4a5ce09cb5b7cf9f35745feddf0166e
Diana Chang <diana.chang.twn@gmail.com>
2016-02-16 14:54:16 -0500

    base conversion, converting int to string, added problem description

--- Awkward Digits.cpp
+++ Awkward Digits.cpp
@@ -1,5 +1,17 @@
 //base conversion, storing digits as char
-
+/*
+Bessie the cow is just learning how to convert numbers between different
+bases, but she keeps making errors since she cannot easily hold a pen
+between her two front hooves.  
+Whenever Bessie converts a number to a new base and writes down the result,
+she always writes one of the digits wrong.  For example, if she converts
+the number 14 into binary (i.e., base 2), the correct result should be
+"1110", but she might instead write down "0110" or "1111".  Bessie never
+accidentally adds or deletes digits, so she might write down a number with
+a leading digit of "0" if this is the digit she gets wrong.
+Given Bessie's output when converting a number N into base 2 and base 3,
+please determine the correct original value of N (in base 10). You can
+assume N is at most 1 billion, and that there is a unique solution for N.*/
 #include <bits/stdc++.h>
 using namespace std;
 int convertFromBaseN(char *s, int n){
