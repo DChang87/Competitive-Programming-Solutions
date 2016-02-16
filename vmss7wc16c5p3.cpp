0e5788caa37d908088633b2b4275d3fa5eb7e126
Diana Chang <diana.chang.twn@gmail.com>
2016-02-16 10:08:26 -0500

    longest path in undirected tree, added problem statement

--- vmss7wc16c5p3.cpp
+++ vmss7wc16c5p3.cpp
@@ -1,3 +1,6 @@
+/*
+print the longest path between any two nodes in the tree.
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int N,a,b;
@@ -9,9 +12,9 @@ int main()
     cin.sync_with_stdio(0); cin.tie(0);
     cin>>N;
     for (int i=0;i<N-1;i++){
+        //describes the paths 
         cin>>a>>b;
-        paths[a].push_back(b);
-        paths[b].push_back(a);
+        paths[a].push_back(b); paths[b].push_back(a);
     }
     queue<pair<int,int>> q;
     q.push(make_pair(1,0));
@@ -19,9 +22,7 @@ int main()
     int topc,topx;
     int farthest=1;
     while (!q.empty()){
-        topx = q.front().first;
-        topc = q.front().second;
-        q.pop();
+        topx = q.front().first; topc = q.front().second;q.pop();
         topc+=1;
         if (topc>MAX){
             MAX = topc;
@@ -35,16 +36,12 @@ int main()
             }
         }
     }
-
     queue<pair<int,int>> q2;
     q2.push(make_pair(farthest,0));
     MAX=0;
     memset(visited,false, sizeof visited);
     while (!q2.empty()){
-        topx = q2.front().first;
-        topc = q2.front().second;
-        q2.pop();
-
+        topx = q2.front().first;topc = q2.front().second; q2.pop();
         topc+=1;
         MAX = max(MAX,topc);
         if (visited[topx])continue;
@@ -55,7 +52,5 @@ int main()
             }
         }
     }
-    cout<<MAX-1<<endl;
-    return 0;
+    cout<<MAX-1<<endl; return 0;
 }
-
